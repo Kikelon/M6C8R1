4e0ca78709bb7502cbbe27e19da4b71298142ca8
Enrique Castrillo <correo_enrique@hotmail.com>
2021-11-29 13:08:00 +0100

    App funcionando

---
+++ apiRest/apiRestDisco.js
@@ -0,0 +1,130 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+let mysql = require("mysql2");
+let connection = mysql.createConnection(
+    {
+        host         : "localhost",
+        user         : "root",
+        password     : "1234567",
+        database     : "angular"
+    }); 
+
+connection.connect(function(error){
+    if(error){
+       console.log(error);
+    }else{
+       console.log('Conexion correcta.');
+    }
+ });
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/discos", 
+        function(request, response)
+        {
+            let sql;
+            sql = "SELECT * FROM discos";
+            console.log(sql);  
+            connection.query(sql, function (err, result) 
+            {
+                if (err){ 
+                    console.log(err);
+                }
+                else 
+                {
+                    console.log(result);
+                    response.send(result);
+                }
+            })
+        }
+        );
+
+app.get("/discos/:id", 
+        function(request, response)
+        {
+            let sql = "SELECT * FROM discos WHERE id=" + request.params.id;
+            console.log(sql);  
+            connection.query(sql, function (err, result) 
+            {
+                if (err){ 
+                    console.log(err);
+                }
+                else 
+                {
+                    console.log(result)
+                    response.send(result);
+                }
+            })
+        }
+        ); 
+        
+app.post("/discos", 
+        function(request, response)
+        {
+            console.log(request.body);
+            let sql = "INSERT INTO discos (titulo, interprete, anyoPublicacion) " + 
+                    "VALUES ('" + request.body.titulo + "', '" + 
+                                  request.body.interprete + "', '" +
+                                  request.body.anyoPublicacion + "')";
+            console.log(sql);                      
+            connection.query(sql, function (err, result) 
+            {
+                if (err) 
+                    console.log(err);
+                else 
+                {
+                    if (result.insertId)
+                        response.send(String(result.insertId));
+                    else
+                        response.send("-1");
+                }
+            })
+        }
+        );         
+
+app.put("/discos", 
+        function(request, response)
+        {
+            console.log(request.body);
+            let params = [request.body.titulo, 
+                          request.body.interprete, 
+                          request.body.anyoPublicacion,
+                          request.body.id]
+            let sql = "UPDATE discos SET titulo = COALESCE(?, titulo) , " + 
+                       "interprete = COALESCE(?, interprete) , " + 
+                       "anyoPublicacion = COALESCE(?, anyoPublicacion)  WHERE id = ?";
+            console.log(sql); 
+            connection.query(sql, params,function (err, result) 
+            {
+                if (err) 
+                    console.log(err);
+                else 
+                {
+                    response.send(result);
+                }
+            })
+        }
+        ); 
+        
+app.delete("/discos", 
+        function(request, response)
+        {
+            console.log(request.body);
+            let sql = "DELETE FROM discos WHERE id = '" + request.body.id + "'";
+            console.log(sql); 
+            connection.query(sql, function (err, result) 
+            {
+                if (err) 
+                    console.log(err);
+                else 
+                {
+                    response.send(result);
+                }
+            })
+        }
+        ); 
+
+ app.listen(3000);
--- src/app/vista/vista.page.ts
+++ src/app/vista/vista.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Disco } from '../models/disco';
+import { DiscoService } from '../shared/disco.service';
 
 @Component({
   selector: 'app-vista',
   templateUrl: './vista.page.html',
   styleUrls: ['./vista.page.scss'],
 })
+
 export class VistaPage implements OnInit {
 
-  constructor() { }
+  public disco: Disco;
+  public toast: any;
+
+  constructor(private apiService: DiscoService, private toastCtrl: ToastController) {
+    this.disco = null;
+  }
+
+  mostrarDisco(discoId: HTMLInputElement){
+    if (discoId.value !== ''){
+        this.apiService.obtenerDisco(parseInt(discoId.value, 10)).subscribe((data: any) =>
+        {
+            this.disco = data[0];
+						const message = 'No hay nigún disco con el id:\"' + discoId.value + '\" en la base de datos';
+						if (data[0]==null) {
+							this.presentToast(message);
+						};
+						discoId.value = '';
+          });
+        }
+      else
+      {
+        this.presentToast('Ha de introducir un id por el que buscar.');
+      }
+  };
+
+  public presentToast(mensaje: string) {
+    this.toast = this.toastCtrl.create({
+      message: mensaje,
+      duration: 2000,
+      position: 'top'
+    }).then((toastData)=>{
+      console.log(toastData);
+      toastData.present();
+    });
+  };
 
   ngOnInit() {
+
   }
 
 }
